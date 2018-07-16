6642daa6edef48b9275cb1d2a1c055d7b3674707
nick-stu <1733763428@qq.com>
2018-07-16 12:52:19 +0800

    update

--- recog.m
+++ recog.m
@@ -1,15 +1,43 @@
-for i=1:10
-	fname = sprintf('ch6\\%db.wav',i-1);
-	x = audioread(fname);
-	[x1 x2] = vad(x);
-	m = mfcc(x);
-	m = m(x1-2:x2-2,:);
-	for j=1:10
-		pout(j) = viterbi(hmm{j}, m);
-	end
-	[d,n] = max(pout);
-	if n ==10
-		n = n-10 ;
-	end
-	fprintf('第%d个词, 识别为%d\n', i-1,n)
+% 	[x1 x2] = vad(x);
+% 	m = mfcc(x);
+% 	m = m(x1-2:x2-2,:);
+% 	for j=1:10
+% 		pout(j) = viterbi(hmm{j}, m);
+% 	end
+% 	[d,n] = max(pout);
+%% 计算阈值
+load('rand.mat')
+result=[];
+for k=1:length(samples{1})
+    m=samples{1}{k};
+    m=mfcc(m);
+    pout = viterbi(hmm{1}, m);
+    result = [result; pout];
 end
+threshold = mean(result)*1.04;
+fprintf('Threshold:%.f\n',threshold);
+%% 计算精度
+load('test.mat')
+result=[];
+for i=1:length(samples)
+    fallnum = 0;
+	for k=1:length(samples{i})
+		m=samples{i}{k};
+        m=mfcc(m);
+        pout = viterbi(hmm{1}, m);
+        result = [result; pout];
+        if pout > threshold
+            fallnum = fallnum + 1;
+        end
+    end
+    fprintf('%d/%d(%.4f)\n',fallnum,length(samples{i}),fallnum/length(samples{i}));
+end
+%% plot
+begin = 0.5;
+figure; plot(result,'k'); hold on;
+for i=1:length(samples)
+    begin = begin + length(samples{i});
+	plot([begin begin],[max(result) min(result)],'r');
+end
+ grid on;
+ plot([0 length(result)],[threshold threshold],'r--');
--- reshapeSample.m
+++ reshapeSample.m
@@ -1,11 +1,108 @@
-outpath='.\hydOutSeg2\';
-for i=1:10
-    load([outpath,'fall - 副本 (',num2str(i),')_1190k.mat']);
-    samples{1}{i} = sum( gccData,1 );
+samples = [];
+%% Out
+outpath='.\outSeg2\';
+for i=1:40
+    load([outpath,'walk - 副本 (',num2str(i),')_1190k.mat']);
+    samples{1}{i} = sum( gccData,1);
 end
 
 for i=1:10
-    load([outpath,'slip - 副本 (',num2str(i),')_1190k.mat']);
-    samples{2}{i} = sum( gccData,1 );
+    load([outpath,'book - 副本 (',num2str(i),')_1190k.mat']);
+    samples{2}{i} = sum( gccData,1);
 end
-save('fallsample','samples');
+
+for i=1:10
+    load([outpath,'jump - 副本 (',num2str(i),')_1190k.mat']);
+    samples{3}{i} = sum( gccData,1);
+end
+
+for i=1:10
+    load([outpath,'push - 副本 (',num2str(i),')_1190k.mat']);
+    samples{4}{i} = sum( gccData,1);
+end
+%% 前期
+outpath='.\newData\hydOutSeg2\';
+for i=1:20
+    load([outpath,'fall - 副本 (',num2str(i),')_1190k.mat']);
+    samples{5}{i} = sum(gccData,1);
+end
+outpath='.\newData\lshOutSeg2\';
+for i=1:20
+    load([outpath,'fall - 副本 (',num2str(i),')_1190k.mat']);
+    samples{6}{i} = sum( gccData,1);
+end
+
+outpath='.\newData\chjOutSeg2\';
+for i=1:20
+    load([outpath,'fall - 副本 (',num2str(i),')_1190k.mat']);
+    samples{7}{i} = sum( gccData,1);
+end
+
+outpath='.\newData\zyhOutSeg2\';
+for i=1:20
+    load([outpath,'fall - 副本 (',num2str(i),')_1190k.mat']);
+    samples{8}{i} = sum( gccData,1);
+end
+%% 后期
+outpath='.\newData\cwqOutSeg2\';
+for i=1:20
+    load([outpath,'fall - 副本 (',num2str(i),')_1190k.mat']);
+    samples{9}{i} = sum( gccData,1);
+end
+outpath='.\newData\lmyOutSeg2\';
+for i=1:12
+    load([outpath,'fall - 副本 (',num2str(i),')_1190k.mat']);
+    samples{10}{i} = sum( gccData,1);
+end
+outpath='.\newData\samOutSeg2\';
+for i=1:17
+    load([outpath,'fall - 副本 (',num2str(i),')_1190k.mat']);
+    samples{11}{i} = sum( gccData,1);
+end
+outpath='.\newData\yqOutSeg2\';
+for i=1:20
+    load([outpath,'fall - 副本 (',num2str(i),')_1190k.mat']);
+    samples{12}{i} = sum( gccData,1);
+end
+outpath='.\newData\cstOutSeg2\';
+for i=1:20
+    load([outpath,'fall - 副本 (',num2str(i),')_1190k.mat']);
+    samples{13}{i} = sum( gccData,1);
+end
+%% 跳步
+% outpath='.\newData\cstWalkOutSeg2\';
+% for i=1:10
+%     load([outpath,'walk - 副本 (',num2str(i),')_1190k.mat']);
+%     samples{14}{i} = sum( gccData,1);
+% end
+% outpath='.\newData\cwqWalkOutSeg2\';
+% for i=1:10
+%     load([outpath,'walk - 副本 (',num2str(i),')_1190k.mat']);
+%     samples{15}{i} = sum( gccData,1);
+% end
+% outpath='.\newData\samWalkOutSeg2\';
+% for i=1:5
+%     load([outpath,'walk - 副本 (',num2str(i),')_1190k.mat']);
+%     samples{16}{i} = sum( gccData,1);
+% end
+% outpath='.\newData\yqWalkOutSeg2\';
+% for i=1:16
+%     load([outpath,'walk - 副本 (',num2str(i),')_1190k.mat']);
+%     samples{17}{i} = sum( gccData,1);
+% end
+
+save('test','samples');
+
+%% Train
+samples = [];
+outpath='.\newData\lshOutSeg2\';
+for i=1:10
+    load([outpath,'fall - 副本 (',num2str(i),')_1190k.mat']);
+    samples{1}{i} = sum(gccData,1);
+end
+outpath='.\newData\cstOutSeg2\';
+for i=1:10
+    load([outpath,'fall - 副本 (',num2str(i),')_1190k.mat']);
+    samples{1}{i+10} = sum(gccData,1);
+end
+save('samples','samples');
