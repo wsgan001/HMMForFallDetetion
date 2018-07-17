12ae9dc5e41614957e189e895aed4f5801c7d432
nick-stu <1733763428@qq.com>
2018-07-17 22:55:56 +0800

    dynamic threshold based on accuracy

--- recog.m
+++ recog.m
@@ -1,4 +1,39 @@
-threshold = -4721;
+clc;
+gap = 25;
+load('test.mat')
+fall=[]; other=[];
+for i=1:length(samples)
+	for k=1:length(samples{i})
+		m=samples{i}{k};
+        m=mfcc(m);
+        pout = viterbi(hmm{1}, m);
+        if i>=5 && i<=14
+        	fall = [fall; pout];
+        else
+            other = [other; pout];
+        end
+    end
+end
+%% 确定最佳阈值
+bestT = 0;
+leastError = Inf;
+minFall = min(fall);
+maxOther = max(other);
+if minFall >= maxOther
+    bestT = (minFall+maxOther)/2;
+else
+    threshold = maxOther;
+    while threshold > minFall
+        threshold = threshold - gap;
+        fallError = length( find(fall<threshold));
+        otherError = length( find(other>threshold));
+        if leastError>fallError+otherError
+            leastError=fallError+otherError;
+            bestT = threshold;
+        end
+    end
+end
+threshold = bestT;
 %% 计算精度
 load('test.mat')
 result=[];
@@ -13,14 +48,18 @@ for i=1:length(samples)
             fallnum = fallnum + 1;
         end
     end
-    fprintf('%d:  %d/%d(%.4f)\n',i,fallnum,length(samples{i}),fallnum/length(samples{i}));
+    fprintf('%f\n',fallnum/length(samples{i}));
 end
 %% plot
 begin = 0.5;
-figure; plot(result,'k'); hold on;
+line = 1:length(result);
+figure; scatter(line,result,'filled'); hold on;
 for i=1:length(samples)
     begin = begin + length(samples{i});
 	plot([begin begin],[max(result) min(result)],'r');
+    if i==4 || i==14
+        plot([begin begin],[max(result) min(result)],'b--');
+    end
 end
  grid on;
  plot([0 length(result)],[threshold threshold],'r--');
